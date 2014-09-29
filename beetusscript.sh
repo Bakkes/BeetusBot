70dfc79456d539f4dab8a4ce08b17af86e875d2e
Jappie Klooster <jappieklooster@hotmail.com>
2014-09-29 11:58:54 +0800

    script no longer uses an absolute path to a user called beetus both, instead it finds its own location and goes from there. This is better than using relative paths because that won't work if you launch the script from a different directory

--- beetusscript.sh
+++ beetusscript.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
+#find the directory of the script
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
 
-LOG_FILE="/home/beetus/beetuslog/log-`date +%Y-%m-%d`.txt"
+#dir contains now the directory of the script
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+LOG_FILE="$DIR/beetuslog/log-`date +%Y-%m-%d`.log"
 DATE=DATE=`date +"%T"`
 
-cd /home/beetus/BeetusBot
+# if in the current directory the beetus log folder does not exists, make it
+[ ! -d $DIR/beetuslog ] && mkdir $DIR/beetuslog && echo "creating log directory"
 
+# create the log file (could have used touch)
 echo $DATE >> $LOG_FILE
-/usr/bin/python /home/beetus/BeetusBot/main.py >> $LOG_FILE 2>&1
+
+# disown the actual script and output the results to a log file
+# ie the current shell is released and another process is forked to execute beetusbot
+python $DIR/main.py >> $LOG_FILE 2>&1
 echo "--------------" >> $LOG_FILE
 exit
 
